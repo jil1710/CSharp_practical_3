5fc2f2b2521aeb396d8f31c7d90b0ad4f54564d9
rider <jil.p@simformsolutions.com>
2023-05-03 13:41:35 +0530

    add: Csharp Abstraction practical 3.3

---
+++ CSharp_Abstraction_practical/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+
+class Laptop
+{
+    private string? brand;
+
+    private string? model;
+
+    public string SetAndGetBrand { private get => brand; set => value = brand = value; }
+    public string SetAndGetModel { private get => model; set => value = model = value; }
+
+    public void LaptopDetails()
+
+    {
+
+        Console.WriteLine("Brand: " + brand);
+
+        Console.WriteLine("Model: " + model);
+
+    }
+
+    private void MotherBoardInfo()
+
+    {
+
+        Console.WriteLine("MotherBoard Information");
+
+    }
+
+    public static void Main(string[] args)
+    {
+        Laptop laptop = new Laptop();
+        laptop.SetAndGetBrand = "HP";
+        laptop.SetAndGetModel = "HP Spectre x360";
+        laptop.LaptopDetails();
+        laptop.MotherBoardInfo();
+        Console.ReadKey();
+    }
+}
