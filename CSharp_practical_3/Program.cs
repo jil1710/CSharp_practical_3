3639cf67251fb8c7300fb23a93998f7a02f2c249
rider <jil.p@simformsolutions.com>
2023-05-03 11:37:33 +0530

    add: CSharp_practical_3.1

---
+++ CSharp_practical_3/Program.cs
@@ -0,0 +1,22 @@
+﻿class Sponsor
+{
+    protected string owner = "Rakuteen";
+}
+
+class Team : Sponsor
+{
+    private string? teamName;
+
+    public void PrintInfo()
+    {
+        Console.WriteLine(string.Concat(owner," ",teamName));
+    }
+
+    public static void Main(string[] args)
+    {
+        Team s = new Team();
+        s.teamName = "ASP.NET";
+        s.PrintInfo();
+        Console.ReadKey();
+    }
+}
