1a4af2ba99fa9cd798815367d293674b4dc2545c
rider <jil.p@simformsolutions.com>
2023-05-03 11:44:22 +0530

    add: CSharp polymorphism practical 3.2

---
+++ CSharp_practical_Polymorphism/Program.cs
@@ -0,0 +1,24 @@
+﻿class Bird
+{
+    public void Voice()
+    {
+        Console.WriteLine("Turr Turr!");
+    }
+}
+
+class Duck : Bird
+{
+    public void Voice()
+    {
+        Console.WriteLine("Quack Quack!");
+    }
+
+    public static void Main(string[] args)
+    {
+        Bird bird = new Bird();
+        Duck duck = new Duck();
+        bird.Voice(); // Output: Turr Turr!
+        duck.Voice(); // Output: Quack Quack!
+        Console.ReadKey();  
+    }
+}
